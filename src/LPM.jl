fd5651a9b394d742147a5751fac46d488a6efbc0
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-19 08:29:50 +0000

    next release

--- src/LPM.jl
+++ src/LPM.jl
@@ -1,9 +1,10 @@
 module LPM
     export LPMVERSION
 
-    const LPMVERSION = r"0.6.1"
+    const LPMVERSION = r"0.6.2"
 
     include("./lpm/ParamTypes.jl")
+    include("./lpm/ModelAPI.jl")
     include("./lpm/Demography.jl")
 
 end # LoneParentsModel
