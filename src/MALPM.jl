8ef16b772fbaa097470c64c1202a3d0e211fb771
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-25 16:28:46 +0000

    move to LPM.jl

--- src/MALPM.jl
+++
@@ -1,9 +0,0 @@
-""" 
-Main components of LoneParentsModel simulation based on MultiAgents.jl package 
-""" 
-module MALPM
-
-include("./malpm/Models.jl")
-include("./malpm/Demography.jl")
-    
-end # MALPM
