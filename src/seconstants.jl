422b341df123ebfe4a638d0cc3970cf8324c832c
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-28 17:01:13 +0000

    SEVersion 0.1.1

--- src/seconstants.jl
+++ src/seconstants.jl
@@ -6,6 +6,6 @@ Temporary simple solution till more standard ways are followed
 
 const SESRCPATH = @__DIR__ 
 const SEPATH    = SESRCPATH * "/.." 
-const SEVERSION = v"0.1.0"
+const SEVERSION = v"0.1.1"
 const XAGENTS_GENERIC_PATH = SESRCPATH * "/generic" 
 const XAGENTS_MA_PATH      = SESRCPATH * "/multiagents"
