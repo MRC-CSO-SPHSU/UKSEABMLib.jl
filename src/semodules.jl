669dfb0cd240d95e85edd1b6ac492a6ea6d6c4db
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-29 09:50:23 +0000

    not needed

--- src/semodules.jl
+++ src/semodules.jl
@@ -4,7 +4,6 @@ Submodules within the SocioEconomics[X] libraries
 This file is included within the SocioEconomics[X] libraries  
 """
 
-include("./SE.jl")
 include("./socioeconomics/ParamTypes.jl")
 include("./socioeconomics/ModelAPI.jl")
 include("./socioeconomics/Demography.jl")
