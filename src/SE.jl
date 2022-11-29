9e41ebc0576d6cd7e1d0f09d1a33d04a5a64afad
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-29 09:02:03 +0000

    some trials

--- src/SE.jl
+++ src/SE.jl
@@ -2,9 +2,9 @@
 if XAGENTS_GENERIC_PATH in LOAD_PATH && XAGENTS_MA_PATH in LOAD_PATH
     error("XAgents configuration error") 
 elseif XAGENTS_MA_PATH in LOAD_PATH
-    using SocioEconomics
-    const SE = SocioEconomics
+    #using SocioEconomics
+    #const SE = SocioEconomics
 else 
-    using SocioEconomicsX
-    const SE = SocioEconomicsX
+    #using SocioEconomicsX
+    #const SE = SocioEconomicsX
 end 
