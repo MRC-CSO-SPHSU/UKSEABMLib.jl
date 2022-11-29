4327c4ef85799ca6cf62216eb5e3de316efe224d
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-29 08:12:16 +0000

    simplification

--- src/SocioEconomics.jl
+++ src/SocioEconomics.jl
@@ -3,18 +3,18 @@ SocioEconomics Library dependent on MultiAgents.jl
 
 pre-request: MultiAgents.jl should be loadable or within LOAD_PATH 
 """
+
 module SocioEconomics
 
-    using MultiAgents: MAVERSION 
-    export verifyCompatibleMAVERSION
+    # ensuring consistent version of MultiAgents.jl library
+    using MultiAgents: MAVERSION
+    @assert MAVERSION == v"0.3.1"  
 
     include("seconstants.jl")
 
     if ! (XAGENTS_MA_PATH in LOAD_PATH) 
         push!(LOAD_PATH, XAGENTS_MA_PATH)
     end 
-
-    @assert MAVERSION == v"0.3.1"   # ensure MultiAgents.jl latest update 
 
     include("semodules.jl")
 
