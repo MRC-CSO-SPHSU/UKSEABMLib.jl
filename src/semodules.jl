a9d866d5a35878a6d6982aca5458755a7d2deda1
Atiyah <a.m.g.elsheikh@gmail.com>
2022-12-05 17:48:36 +0000

    renaming module Demography to Specification

--- src/semodules.jl
+++ src/semodules.jl
@@ -6,4 +6,4 @@ This file is included within the SocioEconomics[X] libraries
 
 include("./socioeconomics/ParamTypes.jl")
 include("./socioeconomics/API.jl")
-include("./socioeconomics/Demography.jl")
+include("./socioeconomics/Specification.jl")
