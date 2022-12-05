6b3b9065d912a11b7c4d6bc8c673020d87ef61b0
Atiyah <a.m.g.elsheikh@gmail.com>
2022-12-05 16:32:24 +0000

    unified initialize interface

--- src/socioeconomics/Demography.jl
+++ src/socioeconomics/Demography.jl
@@ -1,5 +1,26 @@
 module Demography
 
+using ..XAgents: AbstractXAgent
+export AbsProc, AbsPort, initialConnect!
+
+"""
+to be used as a trait for generic functions implementing 
+    candidates of processes
+"""
+abstract type AbsProcess end 
+
+"""two components with identical port are connectable"""
+abstract type AbsPort <: AbsProcess end 
+
+"Interface" 
+function initialConnect!() end 
+
+"symmetry of connections"
+initialConnect!(x,y,pars) = initialConnect!(y,x,pars) 
+
+initialConnect!(x,y,pars,initPort::PType)  where PType <: AbsPort = 
+    initialConnect!(y,x,pars,initPort)
+
 include("./demography/Create.jl") 
 include("./demography/Initialize.jl")  
 include("./demography/Simulate.jl")   
