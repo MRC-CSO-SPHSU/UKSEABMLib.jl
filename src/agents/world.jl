38d02a98a293d82c2f36c8c847431e9e416ee6c3
Atiyah <a.m.g.elsheikh@gmail.com>
2022-12-21 09:21:10 +0000

    blue style issue

--- src/agents/world.jl
+++ src/agents/world.jl
@@ -6,11 +6,9 @@ export adjacent8Towns, findHousesInTown, emptyHouses, emptyHousesInTown
 "Find all towns adjacent to `town` (von Neumann neighbourhood). Memoized for efficiency - empty cache when topology changes."
 @memoize adjacent8Towns(town, towns) = [ t for t in towns if isAdjacent8(town, t) ] 
 
-
 "Find all houses belonging to a specific town. Memoization should make this really fast after a few iterations. Empty cache only when houses appear/disappear."
 @memoize findHousesInTown(t, allHouses) = 
     [ house for house in allHouses if town(house) == t ]
-
 
 emptyHouses(allHouses)  = [ house for house in allHouses if isEmpty(house) ]
 
