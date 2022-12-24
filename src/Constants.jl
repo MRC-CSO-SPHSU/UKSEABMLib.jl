296d0cb4be66ecfc0e8e162659031b9e9fd9eff6
Atiyah <a.m.g.elsheikh@gmail.com>
2022-12-24 12:05:26 +0000

    Version 0.2.6

--- src/Constants.jl
+++ src/Constants.jl
@@ -11,7 +11,7 @@ module Constants
 
 const SESRCPATH = @__DIR__ 
 const SEPATH    = SESRCPATH * "/.." 
-const SEVERSION = v"0.2.5"    # Tuning simulation interfaces 
+const SEVERSION = v"0.2.6"    # re-implementation of town data structure & moving people to new house  
 const XAGENTS_GENERIC_PATH = SESRCPATH * "/generic" 
 const XAGENTS_MA_PATH      = SESRCPATH * "/multiagents"
 
