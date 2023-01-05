046a3f29b572b81cf271002fabfce60ca77e86a2
Atiyah <a.m.g.elsheikh@gmail.com>
2023-01-05 11:28:51 +0000

    additional function for the new simulate interface

--- src/socioeconomics/specification/SimulateNew.jl
+++ src/socioeconomics/specification/SimulateNew.jl
@@ -5,11 +5,15 @@ Functions used for demography simulation
 # to replace the module Simulate
 module SimulateNew 
 
+using ....API.Traits
+import ....API.Traits: verbosemsg 
+
 # API for accessory functions 
 using ....API.ModelFunc  # Here it is does not make sense anymore to 
                     #     employ explicit using statements 
                     #     anything(model) comes from there 
-using ....API.Traits
+import ....API.ModelFunc: select_population, selectedfor
+
 using ....Utilities
 using ....XAgents
 using ....ParamTypes
