1451e0e331d458c61469304734cbd659479071c1
Atiyah <a.m.g.elsheikh@gmail.com>
2023-05-30 13:51:18 +0100

    agent.jl

--- tests/helpers.jl
+++ tests/helpers.jl
@@ -1,5 +1,5 @@
 
-using MultiAgents: MAVERSION, init_majl
+using MultiAgents: MAVERSION, init_majl, verify_agentsjl
 using SocioEconomics: SEVERSION
 using SocioEconomics.XAgents
 using SocioEconomics.ParamTypes
